2fe3968ebd92372cdae18a89a5b972e2d9fa88d7
JAEINz <mangowodls@gmail.com>
2023-04-04 13:56:31 +0900

    fix: 필요 없는 코드, 파일 삭제

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -8,9 +8,7 @@ import {
 import { BoardService } from './board.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
-import { BoardGetDelete } from './entities/board-get,delete.entity';
-import { BoardPost } from './entities/board-post.entity';
-import { Board } from '@prisma/client';
+
 
 
 @ApiTags('Board API')
@@ -39,8 +37,8 @@ export class BoardController {
 
   @Patch(':id')
   @ApiOperation({ summary: '게시글 수정 API' })
-  update(@Param('id') id: string, @Body() updateCrudDto: UpdateBoardDto) {
-    return this.boardService.update(+id, updateCrudDto);
+  update(@Param('id') id: string, @Body() UpdateBoardDto: UpdateBoardDto) {
+    return this.boardService.update(+id, UpdateBoardDto);
   }
 
   @Delete(':id')
